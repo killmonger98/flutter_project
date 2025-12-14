ee87b807ac30d64fd49423cddf2794418ce3afb3
Abhiram <akambadi.nair@gmail.com>
2025-12-14 12:37:24 +0530

    Code refactoring

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../viewmodels/home_viewmodel.dart';
+import '../models/post.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -9,56 +10,114 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => HomeViewModel()..fetchPosts(),
-      child: Scaffold(
+      child: const Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text("Home", style: TextStyle(color: Colors.deepPurple)),
-          backgroundColor: Colors.white,
-        ),
-        body: Consumer<HomeViewModel>(
-          builder: (context, viewModel, child) {
-            if (viewModel.isLoading) {
-              return const Center(
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            }
+        appBar: HomeAppBar(),
+        body: HomeBody(),
+      ),
+    );
+  }
+}
 
-            if (viewModel.error != null) {
-              return Center(child: Text('Error: ${viewModel.error}'));
-            }
+class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomeAppBar({super.key});
 
-            if (viewModel.posts.isEmpty) {
-              return const Center(
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text('No posts found.'),
-                ),
-              );
-            }
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text("Home", style: TextStyle(color: Colors.deepPurple)),
+      backgroundColor: Colors.white,
+    );
+  }
 
-            return ListView.builder(
-              itemCount: viewModel.posts.length,
-              itemBuilder: (context, index) {
-                final post = viewModel.posts[index];
-                return Card(
-                  margin: const EdgeInsets.all(10.0),
-                  child: ListTile(
-                    leading: Image.asset("assets/images/icon.png"),
-                    title: Text(post.name),
-                    subtitle: Text(
-                      post.body,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                );
-              },
-            );
-          },
-        ),
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<HomeViewModel>(
+      builder: (context, viewModel, child) {
+        if (viewModel.isLoading) return const HomeLoadingView();
+        if (viewModel.error != null) {
+          return HomeErrorView(error: viewModel.error!);
+        }
+        if (viewModel.posts.isEmpty) return const HomeEmptyView();
+        return HomePostsList(posts: viewModel.posts);
+      },
+    );
+  }
+}
+
+class HomeLoadingView extends StatelessWidget {
+  const HomeLoadingView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class HomeErrorView extends StatelessWidget {
+  final String error;
+  const HomeErrorView({required this.error});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Error: $error'));
+  }
+}
+
+class HomeEmptyView extends StatelessWidget {
+  const HomeEmptyView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Text('No posts found.'),
+      ),
+    );
+  }
+}
+
+class HomePostsList extends StatelessWidget {
+  final List<Post> posts;
+  const HomePostsList({required this.posts});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        final post = posts[index];
+        return HomePostCard(post: post);
+      },
+    );
+  }
+}
+
+class HomePostCard extends StatelessWidget {
+  final Post post;
+  const HomePostCard({required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10.0),
+      child: ListTile(
+        leading: Image.asset("assets/images/icon.png"),
+        title: Text(post.name),
+        subtitle: Text(post.body, maxLines: 2, overflow: TextOverflow.ellipsis),
       ),
     );
   }
